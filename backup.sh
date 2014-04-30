468571d614eb6d38b2ccc37d42f202e41e6a7955
= <=>
2014-04-30 14:52:16 +0100

    Added backup.sh, a simple shell script to compress a folder and save in a destination. Usefull for automated backups

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/basg
+#Purpose = Backups
+#Created on 31-04-2014
+#Author = Guilherme Cardoso
+#Version 0.1
+
+#START
+
+TIME=`date +"%d-%m-%Y-%H-%M-%S"` # day-month-year-hour-minute-second. Ie: 30-04-2014-14-49-47
+FILENAME="backup-$TIME.tar.gz" # file name is combined with the date
+SRCDIR="/var/www" # source directory
+DESDIR="/var/backups" # destination directory
+tar -cpzf $DESDIR/$FILENAME $SRCDIR # compress the source directory and saves the compress archive in destination
+
+#END
